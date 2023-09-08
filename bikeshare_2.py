8535ccbdb585ee166d6291a75d2678226f036d12
QueensleyC <ebyqueensley1@gmail.com>
2023-09-08 01:58:39 +0100

    Add descriptive comment to get_filters function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -7,6 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+# This function prompts and authenticates user input
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
